6ff0886c1e391acdeb80ed0b2b94d57306814c67
Kirisame <wuyuhuahen@gmail.com>
2024-01-18 15:17:26 +0900

    0.1.4 build3

--- src/actions/createDefinition.ts
+++ src/actions/createDefinition.ts
@@ -3,23 +3,13 @@
 import prisma from "@/actions/db";
 
 const createDefinition = async (meaningId: number, definition: string = "Input a definition", example?: string) => {
-    const maxOrder = await prisma.definitions.aggregate({
-        _max: {
-            order: true,
-        },
-        where: {
-            meaning_id: meaningId,
-        },
-    });
-
-    const newOrder = (maxOrder._max.order || 0) + 1;
 
     return prisma.definitions.create({
         data: {
             meaning_id: meaningId,
             definition: definition,
             example: example,
-            order: newOrder,
+            order: null,
             created_at: new Date(),
         }
     })
